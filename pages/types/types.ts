32cad26288224d05c49ad32aca6bad0f88317469
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-07-14 22:26:03 -0300

    refactor: types

--- pages/types/types.ts
+++ pages/types/types.ts
@@ -1,7 +1,20 @@
+import React, { ReactNode } from 'react'
+
 export interface IBenefits {
   image?: string
   title: string
   description: string
   discount: string
   rules: string
+}
+
+export interface ContextChildrenProps {
+  children: ReactNode
+}
+
+export interface UrbisContextProps {
+  benefitList: IBenefits[] | []
+  setBenefitList: React.Dispatch<React.SetStateAction<never[]>>
+  showModal: boolean
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>
 }
