417ee24c3c2b6ca5145497bd125d9e2a63cbe8a6
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-07-14 22:25:51 -0300

    feat: context

---
+++ pages/context/context.tsx
@@ -0,0 +1,26 @@
+import { createContext, useContext, useState } from 'react'
+import { ContextChildrenProps, UrbisContextProps } from '../types/types'
+
+export const defaultValues: UrbisContextProps = {
+  benefitList: [],
+  setBenefitList: () => [],
+  showModal: false,
+  setShowModal: () => false
+}
+
+export const UrbisContext = createContext<UrbisContextProps>(defaultValues)
+
+export const useUrbisContext = () => useContext(UrbisContext)
+
+export const UrbisContextProvider = ({ children }: ContextChildrenProps) => {
+  const [benefitList, setBenefitList] = useState([])
+  const [showModal, setShowModal] = useState(false)
+
+  return (
+    <UrbisContext.Provider
+      value={{ benefitList, setBenefitList, showModal, setShowModal }}
+    >
+      {children}
+    </UrbisContext.Provider>
+  )
+}
