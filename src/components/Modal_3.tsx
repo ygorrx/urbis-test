7e5217279f4fee27ee3ba66add0ad1f913e35b82
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-07-16 11:08:06 -0300

    feat: Modal third logic implemented

--- src/components/Modal_3.tsx
+++ src/components/Modal_3.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-import styles from '../../styles/Modal.module.css'
+import styles from '../../styles/ModalThird.module.css'
 
 import { useUrbisContext } from '../context/context'
+
+import Image from 'next/image'
 
 export function ModalThird() {
   const { setModalStep, setShowModal } = useUrbisContext()
@@ -15,15 +17,22 @@ export function ModalThird() {
   return (
     <div className={styles.modal_background}>
       <div className={styles.modal_container}>
-        <div className={styles.title_close_btn}>
-          <button onClick={handleModalClose}>X</button>
+        <div className={styles.image_icon}>
+          <Image src={'/assets/medal.svg'} width={60} height={60} />
         </div>
         <div className={styles.title}>
-          <h1>Step third</h1>
+          <h1>Sucesso!</h1>
         </div>
         <div className={styles.body}>
-          <button onClick={handleModalClose}>Fechar</button>
+          <p>
+            Sua resposta foi resgistrada com sucesso. Agradecemos muito por você
+            ter dedicado esse tempo para nos ajudar a tornar nosso clube cada
+            vez melhor.
+          </p>
         </div>
+        <button className={styles.btn_save} onClick={handleModalClose}>
+          Fechar
+        </button>
       </div>
     </div>
   )
